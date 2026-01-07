0d14a9b0a6e92ac62041ee910cdff1cc25867eab
cookteam <tou@robo.lab.uec.ac.jp>
2026-01-07 15:55:55 +0900

    feat: add trimesh boolean operation test script

---
+++ trimesh_bool_test.py
@@ -0,0 +1,17 @@
+import trimesh
+
+if __name__ == "__main__":
+	base_mesh = trimesh.load("debug_pc_calc_data/base_mesh.obj")
+	knife_mesh = trimesh.load("debug_pc_calc_data/knife_mesh.obj")
+
+	# make box from base_mesh's aabb.
+	box = trimesh.creation.box(bounds=base_mesh.bounds)
+	# move box up by 0.01 in z direction
+	knife_mesh.apply_translation([0, 0, -0.01])
+
+	merged = trimesh.util.concatenate([base_mesh, knife_mesh])
+
+	intersection = base_mesh.intersection(knife_mesh, engine='blender', check_volume=False, use_exact=True)
+	print(intersection.is_empty)
+	trimesh.exchange.export.export_mesh(intersection, "debug_pc_calc_data/contact_mesh.obj")
+	trimesh.exchange.export.export_mesh(merged, "debug_pc_calc_data/merged.obj")
