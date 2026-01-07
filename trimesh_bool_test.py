8096dade304f5020b5645c33cc6b60d95f54f497
cookteam <tou@robo.lab.uec.ac.jp>
2026-01-07 20:47:29 +0900

    fix: add normal and face orientation handling for base mesh using Open3D

--- trimesh_bool_test.py
+++ trimesh_bool_test.py
@@ -1,7 +1,7 @@
 import importlib, os
 os.environ["PATH"] += os.pathsep + "/home/cookteam/Documents/blender-4.5.3-linux-x64"
 
-
+import open3d as o3d
 import trimesh
 import trimesh.interfaces.blender as blend
 importlib.reload(blend)
@@ -10,6 +10,18 @@ importlib.reload(blend)
 if __name__ == "__main__":
 	base_mesh = trimesh.load("debug_pc_calc_data/base_mesh.obj")
 	knife_mesh = trimesh.load("debug_pc_calc_data/knife_mesh.obj")
+
+	# Normal & Face orientation must be fixed for boolean to work correctly.
+	
+	print(f"Is base_mesh watertight? {base_mesh.is_watertight}")
+	base_mesh.fix_normals()  # Need networkx package
+	trimesh.exchange.export.export_mesh(base_mesh, "debug_pc_calc_data/base_mesh_trimesh_oriented.obj")
+
+	o3dmesh = o3d.io.read_triangle_mesh("debug_pc_calc_data/base_mesh.obj")
+	o3dmesh.compute_vertex_normals()
+	o3dmesh.compute_triangle_normals()
+	o3dmesh.orient_triangles()
+	o3d.io.write_triangle_mesh("debug_pc_calc_data/base_mesh_oriented.obj", o3dmesh)
 
 	# make box from base_mesh's aabb.
 	box = trimesh.creation.box(bounds=base_mesh.bounds)
